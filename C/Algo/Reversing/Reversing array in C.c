161b80ae6851a51f34d25710fd24e3b86f3e4d29
Uddeshya Tyagi <uddeshyatyagi775@gmail.com>
2020-10-22 17:46:09 +0530

    Added functionality to make program user friendly (#24)

--- C/Algo/Reversing/Reversing array in C.c
+++ C/Algo/Reversing/Reversing array in C.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 int main(){
-    int arr[6] = {3,5,2,7,8,4};
+    int n;
+    scanf("%d",&n);
+    int arr[n];
     int i,j,t;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
     // Reversing the array by swaping the numbers
-    for(i=0,j=5 ;i<j ;i++,j--){
+    for(i=0,j=n-1 ;i<j ;i++,j--){
             t = arr[i];
             arr[i] = arr[j];
             arr[j] = t;
     }
     // To print the reversed array
-    for(i=0; i<6; i++){
+    for(i=0; i<n; i++){
         printf("%d ",arr[i]);
     }
     return 0;
 }
+
